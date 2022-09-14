f910c73bec86f431740563ae2e0e93a6e951678e
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-09-14 12:05:04 +0545

    feat:fin 5.14 ,issue in username

--- src/components/Blogs.test.js
+++ src/components/Blogs.test.js
@@ -1,16 +1,38 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("renders title and author", () => {
-  const blog = {
-    title: "the first test for frontend",
-    author: "Admin@1234",
+describe("Blog component tests", () => {
+  let blog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
   };
 
-  const component = render(<Blog blog={blog} />);
-  expect(component.container).toHaveTextContent(
-    "the first test for frontend - Admin@1234"
-  );
+  test("renders title and author", () => {
+    const blog = {
+      title: "the first test for frontend",
+      author: "Admin@1234",
+    };
+
+    const component = render(<Blog blog={blog} />);
+    expect(component.container).toHaveTextContent(
+      "the first test for frontend - Admin@1234"
+    );
+  });
+
+  test("clicking the view button displays url and number of likes", async () => {
+    const component = render(<Blog blog={blog} />);
+
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+
+    expect(component.container).toHaveTextContent("https://reactpatterns.com/");
+
+    expect(component.container).toHaveTextContent("7");
+  });
 });
